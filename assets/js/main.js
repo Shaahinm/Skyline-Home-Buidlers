db07fe957d4d8f6d8911170409072f46f27c6c79
Shaahin Maesoumi <shahinm@gmail.com>
2018-03-12 14:50:18 +0330

    project details page added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,21 @@
 $(window).on("scroll", function(e) {
   if ($(this).scrollTop() > 20) {
-      $("#navbar .navbar-wrapper").addClass("scrolled");
+    $("#navbar .navbar-wrapper").addClass("scrolled");
   } else {
     $("#navbar .navbar-wrapper").removeClass("scrolled");
-  }
+  }  
+});
+
+$(function(){
+  $("#btn-send").click(function(e) {
+    e.stopPropagation();
+    var msg = $("#msg").val();
+    var email = $("#email").val();
+    var name = $("#name").val();
+    if (msg.length === 0 || email.length === 0 || name.length === 0) {
+      alert("Please Fill all the fields");
+    } else {
+      window.open(`mailto:test@example.com?subject=${name}&body=${msg}`);
+    }
+  });
 });
